8df5de3c46c3f45681dd75ba74f4324c6db32c2e
404Exist <osamasaad1237@gmail.com>
2021-10-20 10:30:09 +0200

    google clone app

---
+++ src/components/Links.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const links = [
+  { url: '/search', text: '🔎 All'},
+  { url: '/news', text: '🗞️ News'},
+  { url: '/images', text: '📸 Images'},
+  { url: '/videos', text: '📺 Videos'},
+];
+
+export const Links = () => {
+  return (
+    <div className="flex sm:justify-around justify-between items-center mt-4">
+      {links.map(({ url, text }) => (
+        <NavLink to={url} activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2" className="m-2 mb-0"> 
+          {text}
+        </NavLink>
+      ))}
+    </div>
+  )
+}
